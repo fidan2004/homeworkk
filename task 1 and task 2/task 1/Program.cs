8f04d755ce2a1b825b9a20ae8bf2d4c278cb7c08
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2022-08-11 12:23:24 +0400

    Add files via upload

---
+++ task 1 and task 2/task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          //task 1
+            DateTime dt = new DateTime(2004, 05, 29);
+            Console.WriteLine(dt.ToString("dddd"));
+           
+          //task 2
+            DateTime dt2 = DateTime.Now;
+            DateTime dt3 = new DateTime(2004, 05, 29);
+            var result = DateTime.Now - dt3;
+            Console.WriteLine(result);
+        }
+    }
+}
